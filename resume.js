48b4924be22910ed1036642b7017c11949994494
gracesacco22 <165083751+gracesacco22@users.noreply.github.com>
2025-01-30 13:41:17 -0800

    Delete resume.js

--- resume.js
+++
@@ -1,7 +0,0 @@
-const tBtn = document.getElementById('toggle-details');
-const profileDetails = document.querySelector('.card-details');
-tBtn.addEventListener('click', () => {
-    profileDetails.classList.toggle('show-details');
-    tBtn.querySelector('i').classList.toggle('fa-chevron-down');
-    tBtn.querySelector('i').classList.toggle('fa-chevron-up');
-});
